943398843862c7f86fe5fb41e13949731d6619e6
DeshanHarshana <nawarathnadeshan@gmail.com>
2021-07-15 21:20:47 +0530

    BMI calculation added

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -27,27 +27,35 @@ this.bmicolor=this.selectBmiColor(this.BMI);
 
   selectBmiColor(bmi){
     if(bmi<16){
+      this.state="Severe Thinness";
       return "less16";
     }
     if(bmi<17 && bmi>=16){
+      this.state="Moderate Thinness"
       return "less17";
     }
     if(bmi<18.5 && bmi>=17){
+      this.state="Mild Thinness"
       return "less18-5";
     }
     if(bmi<25 && bmi>=18.5){
+      this.state="Normal"
       return "less25";
     }
     if(bmi<30 && bmi>=25){
+      this.state="Overweight"
       return "less30";
     }
     if(bmi<35 && bmi>=30){
+      this.state="Obese Class I"
       return "less35";
     }
     if(bmi<40 && bmi>=35){
+      this.state="Obese Class II"
       return "less40";
     }
-    if(bmi<48 && bmi>=40){
+    if(bmi<49 && bmi>=40){
+      this.state="Obese Class III	"
       return "less48";
     }
   }
